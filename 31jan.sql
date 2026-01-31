af6397a890b4e311d55651d9f70a7f099c8bf146
MD. SABBIR HASAN <sabbirhasan.stat@gmail.com>
2026-01-31 18:36:48 +0600

    31jan

---
+++ 31jan.sql
@@ -0,0 +1,33 @@
+--1
+
+create table e1294187 (
+eid NUMBER(10),
+ename VARCHAR2(30),
+salary NUMBER(15) DEFAULT 1000,
+hire_date DATE DEFAULT SYSDATE,
+address VARCHAR2(50) DEFAULT 'Dhaka',
+CONSTRAINT hr_e1294187_eid_pk PRIMARY KEY(eid)
+);
+
+--2
+
+select department_id, AVG(salary)
+from employees
+HAVING MAX(salary) >12000
+group by department_id;
+
+--3
+
+select first_name
+from employees
+where first_name like 'S%_';
+
+--4
+select avg(salary)
+from employees
+where job_id like 'SA%';
+
+--5
+select First_name, hire_date
+from employees
+order by hire_date;
